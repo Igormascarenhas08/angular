27e9d0f93e68de69737b0a83a6b2868c532de218
Igormascarenhas08 <igor.mascarenha@fbest.org.br>
2025-09-23 15:55:52 -0300

    Finalização da API

---
+++ SmartSchool API/API/Data/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+using API.Controllers;
+using API.Models;
+
+namespace API.Data
+{
+    public interface IRepository//Assinatura dos métodos
+    {
+        //Geral
+
+        void Add<T>(T entity) where T : class;
+        void Update<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        Task<bool> SaveChangesAsync();
+
+        //ALUNO
+        Task<Aluno[]> GetAllAlunosAsync(bool includeProfessor);
+        Task<Aluno[]> GetAlunosAsyncByDisciplinaId(int disciplinaId, bool includeDisciplina);
+        Task<Aluno> GetAlunoAsyncById(int alunoId, bool includeProfessor);
+
+        //PROFESSOR
+        Task<Professor[]> GetAllProfessoresAsync(bool includeAluno);
+        Task<Professor> GetProfessorAsyncById(int professorId, bool includeAluno);
+        Task<Professor[]> GetProfessoresAsyncByAlunoId(int alunoId, bool includeDisciplina);
+    }
+}
---
+++ SmartSchool API/API/Models/AlunoDisciplina.cs
@@ -0,0 +1,25 @@
+﻿
+namespace API.Models
+{
+    public class AlunoDisciplina
+    {
+        public AlunoDisciplina()
+        {
+            
+        }
+
+        public AlunoDisciplina(int alunoId, Aluno aluno, int disciplinaId, Disciplina disciplina)
+        {
+            this.AlunoId = alunoId;
+            this.Aluno = aluno;
+            this.DisciplinaId = disciplinaId;
+            this.Disciplina = disciplina;
+        }
+
+        public int AlunoId { get; set; }
+        public Aluno Aluno { get; set; }
+        public int DisciplinaId { get; set; }
+        public Disciplina Disciplina { get; set; }
+
+    }
+}
